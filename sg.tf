ddc5aa316c444f68a7203a8e3f9af5b8b3300ce0
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-11 02:49:34 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,20 +1,49 @@
-module "Jenkins" {
-  source = "terraform-aws-modules/security-group/aws"
+resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-0ee23bfc74a881de5"
+      key_name = var.key_name
+      instance_type = "t2.micro"
+      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
 
-  name        = "Jenkins-SG"
-  description = "Security group for ssh for custom ports open in VPC"
-  vpc_id      = module.vpc.vpc_id
+ resource "aws_security_group" "sonar-sg-2022" {
+      name        = "security_sonar_group_2022"
+      description = "security group for Sonar"
 
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "ssh-service ports"
-      cidr_blocks = "0.0.0.0/0"
-    },
-]
-egress_rules                                             = ["all-all"]
-  egress_cidr_blocks                                       = ["0.0.0.0/0"]
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+      tags= {
+        Name = "security_sonar"
+      }
+    }
+
+# Create Elastic IP address for Sonar instance
+resource "aws_eip" "mySonarInstance" {
+  vpc      = true
+  instance = aws_instance.mySonarInstance.id
+tags= {
+    Name = "sonar_elastic_ip"
+  }
 }
