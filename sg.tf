80be90376454707d8252b8777d81dea0daf4027c
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-12 22:30:38 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
-module "jenkins" {
+module "Bastion" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "jenkins-SG"
+  name        = "bastion-SG"
   description = "Security group for ssh for custom ports open in VPC"
   vpc_id      = module.vpc.vpc_id
 
