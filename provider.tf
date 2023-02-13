642f2924d9fab6bcf7c3af65b111d579ef95195a
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-13 23:00:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
  region = "us-east-1"
- access_key = "AKIAQBLYN2U3GYAIDWFG"
- secret_key = "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"
+ //access_key = "AKIAQBLYN2U3GYAIDWFG"
+// secret_key = "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"
 }
