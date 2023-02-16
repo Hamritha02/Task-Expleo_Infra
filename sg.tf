69257759d4967dd14315f8afbeadeb8db12ca135
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-16 23:08:37 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -19,23 +19,4 @@ egress_rules                                             = ["all-all"]
   egress_cidr_blocks                                       = ["0.0.0.0/0"]
 }
 
-module "public-SG" {
-  source = "terraform-aws-modules/security-group/aws"
 
-  name        = "public-SG"
-  description = "Security group for ssh for custom ports open in VPC"
-  vpc_id      = module.vpc.vpc_id
-
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "ssh-service ports"
-      cidr_blocks = "0.0.0.0/0"
-    },
-]
-egress_rules                                             = ["all-all"]
-  egress_cidr_blocks                                       = ["0.0.0.0/0"]
-}
