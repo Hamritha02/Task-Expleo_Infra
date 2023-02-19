a492a8033798b8b8c367d66710f56ebe2cfa9954
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-19 21:23:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ module "Node" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "Node-server"  
+  name = "Node-server"    
   
   ami                    = var.image_id
   instance_type          = var.instance_type  
