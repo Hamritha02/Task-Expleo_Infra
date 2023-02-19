42b91dc9c2ead784ee70af6a53617a021044cfb5
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-19 21:32:23 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terrae"
     key    = "home/ubuntu/project/.terraform/terraform.tfstate"
-    region =  var.region  
+    region =  "us-east-1"
     encrypt = true   
   }
 }
