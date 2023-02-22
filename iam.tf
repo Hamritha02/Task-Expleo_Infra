1889a858cc0e5d5d9491d411d6b9c03ea8fb4a00
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-22 20:48:02 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -7,7 +7,7 @@
       environment         = "dev"
       label_order         = ["environment", "name"]
       assume_role_policy  = data.aws_iam_policy_document.default.json
-      managed_policy_arns = arn:aws:iam::002936919350:role/Task-IAMaccess
+      managed_policy_arns = arn:aws:iam::002936919350:instance-profile/Task-IAMaccess
       policy_enabled      = true
       policy              = data.aws_iam_policy_document.iam-policy.json
     }
