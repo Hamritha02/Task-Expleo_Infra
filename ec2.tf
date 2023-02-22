208793c7d46f105fa5f87279ac95eac1b8865080
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-23 01:13:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,9 +11,29 @@ module "Node" {
   vpc_security_group_ids = [module.node-app.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
   //create_iam_instance_profile = true
- // iam_role_description   = "IAM role for EC2 instance"
-  //iam_role_path          = "arn:aws:iam::002936919350:instance-profile/Task-IAMaccess"
+ iam_role_description   = "IAM role for EC2 instance"
+ iam_role_path          = "arn:aws:iam::002936919350:instance-profile/Task-IAMaccess"
    
+ module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  trusted_role_arns = [
+    "arn:aws:iam::002936919350:role/Task-IAMaccess"
+    ]
+
+  create_role = true
+
+  role_name         = "Task_IAMaccess"
+  role_requires_mfa = true
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::002936919350:role/Task-IAMaccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+    "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicFullAccess",
+  ]
+  number_of_custom_role_policy_arns = 4
+}
   tags = {
     Terraform   = "true"
     Environment = "dev"
