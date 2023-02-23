000684e6046fb43975932e4828a4489018b4c192
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-02-23 22:55:04 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,35 @@
+
+module "iam-role" {
+      source              = "clouddrove/iam-role/aws"
+      version             = "1.3.0"
+
+      name                = "iam-role"
+      environment         = "test"
+      label_order         = ["environment", "name"]
+      assume_role_policy  = data.aws_iam_policy_document.default.json
+      managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicFullAccess",
+                             "arn:aws:iam::aws:policy/AdministratorAccess"
+                            ]
+      policy_enabled      = true
+      policy              = data.aws_iam_policy_document.iam-policy.json
+    }
+
+      data "aws_iam_policy_document" "default" {
+      statement {
+      effect  = "Allow"
+      actions = ["sts:AssumeRole"]
+      principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+      }
+      }
+    }
+      data "aws_iam_policy_document" "iam-policy" {
+      statement {
+      actions = [
+      "ecr-public:*",
+      "sts:GetServiceBearerToken" ]
+      effect = "Allow"
+      resources = ["*"]
+      }
+ 
