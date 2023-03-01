76bee9069b85f0bc76a17896eccb2a233d87cd2d
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-03-01 09:27:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ module "Node" {
   key_name               = "node"
   monitoring             = true
   vpc_security_group_ids = [module.node-app.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.public_subnets[0] 
   iam_instance_profile   = "terra-jenkins-instance-profile"
 
   tags = {
